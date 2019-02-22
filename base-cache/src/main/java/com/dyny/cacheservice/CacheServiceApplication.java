4f8f36f757678366f7fcc991b2e27a6f489058e7
lane <wglbsr@163.com>
2019-02-22 14:57:31 +0800

    refracted

---
+++ base-cache/src/main/java/com/dyny/cacheservice/CacheServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dyny.cacheservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheServiceApplication.class, args);
+	}
+
+}
