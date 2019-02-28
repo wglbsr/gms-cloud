9468d53580783dde25b54eae79f31cd801e917f7
lane <wglbsr@163.com>
2019-02-28 15:56:44 +0800

    update 分配端口

--- base-gateway/src/main/java/com/dyny/gateway/controller/AuthController.java
+++ base-gateway/src/main/java/com/dyny/gateway/controller/AuthController.java
@@ -1,12 +1,7 @@
 package com.dyny.gateway.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.dyny.common.utils.BaseController;
-import com.dyny.gateway.api.UserApi;
-import com.dyny.gateway.auth.Token;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,26 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController extends BaseController {
-    @Autowired
-    UserApi userApi;
-
-    @RequestMapping("/login")
-    public String getToken(@RequestParam("username") String username,
-                           @RequestParam("password") String password,
-                           @RequestParam("url") String url) {
-        String result = userApi.login(username, password);
-        JSONObject userInfo = getJsonData(userApi.login(username, password));
-        if (userInfo != null) {
-            Token token = new Token(username, password);
-            JSONObject newData = new JSONObject();
-            newData.put(BaseController.KEY_TOKEN, token.getToken());
-            newData.put(BaseController.KEY_USER_INFO, userInfo);
-            newData.put(BaseController.KEY_EXPIRE_TIME, token.getExpireTime());
-            newData.put(BaseController.KEY_REDIRECT_URI, url);
-            return getSuccessResult(newData);
-        } else {
-            return result;
-        }
-    }
+//    @Autowired
+//    UserApi userApi;
+//
+//    @RequestMapping("/login")
+//    public String getToken(@RequestParam("username") String username,
+//                           @RequestParam("password") String password,
+//                           @RequestParam("url") String url) {
+//        String result = userApi.login(username, password);
+//        JSONObject userInfo = getJsonData(userApi.login(username, password));
+//        if (userInfo != null) {
+//            Token token = new Token(username, password);
+//            JSONObject newData = new JSONObject();
+//            newData.put(BaseController.KEY_TOKEN, token.getToken());
+//            newData.put(BaseController.KEY_USER_INFO, userInfo);
+//            newData.put(BaseController.KEY_EXPIRE_TIME, token.getExpireTime());
+//            newData.put(BaseController.KEY_REDIRECT_URI, url);
+//            return getSuccessResult(newData);
+//        } else {
+//            return result;
+//        }
+//    }
 
 }
