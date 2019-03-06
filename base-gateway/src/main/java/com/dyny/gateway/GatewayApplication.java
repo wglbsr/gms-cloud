5ba98eaae6873149c53a8bd74a5e4727b1ffc333
lane <wglbsr@163.com>
2019-03-06 17:31:33 +0800

    update 1.开始编写用户服务

--- base-gateway/src/main/java/com/dyny/gateway/GatewayApplication.java
+++ base-gateway/src/main/java/com/dyny/gateway/GatewayApplication.java
@@ -2,12 +2,18 @@ package com.dyny.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableFeignClients
--- base-redis/src/main/java/com/dyny/baseredis/BaseRedisApplication.java
+++ base-redis/src/main/java/com/dyny/baseredis/BaseRedisApplication.java
@@ -2,9 +2,15 @@ package com.dyny.baseredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
 public class BaseRedisApplication {
 
